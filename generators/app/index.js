a791b48a8f8a8333077a32abac9fef0e682f2de0
Lucas Gomez <lucas.gomez@tecnilogica.com>
2017-08-31 14:20:18 +0200

    Second iteration

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,25 +1,65 @@
 var Generator = require('yeoman-generator');
 
 module.exports = class extends Generator {
+  paths() {
+    this.sourceRoot();
+    // returns './templates'
+
+    this.templatePath('index.js');
+    // returns './templates/index.js'
+  }
+
   initializing() {
     this.log('Welcome to my JUNGLE');
   }
-  
+
   prompting() {
     return this.prompt([{
       type: 'input',
-      name: 'name',
-      message: 'Your project name',
-      default: 'WTFruck' // Default to current folder name
+      name: 'title',
+      message: 'Your web title',
+      default: 'Home' // Default to current folder name
     }, {
       type: 'confirm',
       name: 'cool',
       message: 'Would you like to enable the Cool feature?',
       store: true
+    }, {
+      type: 'list',
+      name: 'author',
+      message: 'Select your authoring',
+      choices: [
+        'Anonymous',
+        'cool',
+        'Lucas deGomez'
+      ]
+    }, {
+      type: 'checkbox',
+      name: 'files',
+      message: 'Initialize this?',
+      choices: [
+        'JavaScript',
+        'Css'
+      ]
     }]).then((answers) => {
-      this.log('app name', answers.name);
+      this.log('app name', answers.title);
+      this.title = answers.title
       this.log('cool feature', answers.cool);
+      this.author = answers.author;
+      this.log('files?', answers.files);
+      this.options = answers.files;
     });
+  }
+
+  writing() {
+    this.fs.copyTpl(
+      this.templatePath('index.html'),
+      this.destinationPath('index.html'),
+      {
+        title: this.title,
+        author: this.author
+      }
+    );
   }
 
   method1() {
@@ -28,5 +68,9 @@ module.exports = class extends Generator {
 
   end() {
     this.log('Goodbye my friend');
+    this.fs.delete(
+      // cleans up Yeoman rc
+      this.destinationPath('.yo-rc.json')
+    )
   }
 };
