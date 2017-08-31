e95dab96e5c67445e44a2e2e4df56662ce19d00e
Lucas Gomez <lucas.gomez@tecnilogica.com>
2017-08-31 12:07:02 +0200

    First iteration

---
+++ README.md
@@ -0,0 +1,5 @@
+# Creating my first yeoman generator for non-evil purposes
+
+## Instructions
+First create a folder with generator-[name]
+
---
+++ generators/app/index.js
@@ -0,0 +1,31 @@
+var Generator = require('yeoman-generator');
+
+module.exports = class extends Generator {
+  initializing() {
+    this.log('Welcome to my JUNGLE');
+  }
+  
+  prompting() {
+    return this.prompt([{
+      type: 'input',
+      name: 'name',
+      message: 'Your project name',
+      default: 'WTFruck' // Default to current folder name
+    }, {
+      type: 'confirm',
+      name: 'cool',
+      message: 'Would you like to enable the Cool feature?'
+    }]).then((answers) => {
+      this.log('app name', answers.name);
+      this.log('cool feature', answers.cool);
+    });
+  }
+
+  method1() {
+    this.log('method 1 just ran');
+  }
+
+  end() {
+    this.log('Goodbye my friend');
+  }
+};
