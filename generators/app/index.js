7f5b67654fe6e18fbec4048d1ebf5e2cb8d85556
Lucas deGomez <lucasdegomez@gmail.com>
2017-09-04 11:30:21 +0200

    Adds conditional use of normalize

--- generators/app/index.js
+++ generators/app/index.js
@@ -24,29 +24,43 @@ module.exports = class extends Generator {
       name: 'author',
       message: 'Author',
       default: 'Lucas deGomez'
+    }, {
+      type: 'confirm',
+      name: 'normalize',
+      message: 'Would you like to use normalize?'
     }]).then((answers) => {
-      this.title = answers.title
+      this.title = answers.title;
       this.author = answers.author;
+      this.normalize = answers.normalize;
     });
   }
 
   writing() {
+    // Object passed to template
+    let template_info = {
+      title: this.title,
+      author: this.author.answers,
+      normalize: false
+    }
+    // Object builder
+    if(this.normalize) {
+      template_info.normalize = `<link href="css/_normalize.css" rel="stylesheet">`
+    }
     this.fs.copyTpl(
       this.templatePath('index.html'),
       this.destinationPath('index.html'),
-      {
-        title: this.title,
-        author: this.author
-      }
+      template_info
     );
     this.fs.copy(
       this.templatePath('style.css'),
       this.destinationPath('css/style.css')
     );
-    this.fs.copy(
-      this.templatePath('normalize.css'),
-      this.destinationPath('css/_normalize.css')
-    );
+    if(this.normalize) {
+      this.fs.copy(
+        this.templatePath('normalize.css'),
+        this.destinationPath('css/_normalize.css')
+      );
+    }
     this.fs.copy(
       this.templatePath('script.js'),
       this.destinationPath('js/script.js')
